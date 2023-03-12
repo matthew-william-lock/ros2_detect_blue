8a1e8bfea005e24556605efef653373d260d49c8
matthew-william-lock <matthewwilliamlock@gmail.com>
2023-03-12 12:21:15 +0100

    Added parameters and verbose logging option

--- ros2_detect_blue/detect_blue.py
+++ ros2_detect_blue/detect_blue.py
@@ -1,3 +1,32 @@
+# BSD 3-Clause License
+
+# Copyright (c) 2023, Matthew Lock
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import rclpy
 from rclpy.node import Node
 
@@ -17,7 +46,17 @@ class DetectBlueNode(Node):
     def __init__(self):
         super().__init__('detect_blue')
         self.get_logger().info('detect_blue node is starting')
-        self.image_sub = self.create_subscription(Image, '/camera', self.image_callback, 1)
+
+        # Get parameters
+        self.declare_parameter('camera_topic', '/camera')
+        self.declare_parameter('verbose', False)
+
+        self.camera_topic = self.get_parameter('camera_topic').value
+        self.verbose = self.get_parameter('verbose').value
+
+        self.image_sub = self.create_subscription(Image, self.camera_topic, self.image_callback, 1)
+        self.get_logger().info('Subscribed to ' + self.camera_topic)
+
         plt.ion() # Turn on interactive mode
         self.fig, self.axs = plt.subplots(1, 3, figsize=(10, 5))
 
@@ -27,7 +66,7 @@ class DetectBlueNode(Node):
 
         # Declare boolean publisher and time to publish every 1 second
         self.blue_detected = False
-        self.blue_detected_pub = self.create_publisher(Bool, '/blue_detected', 1)
+        self.blue_detected_pub = self.create_publisher(Bool, 'blue_detected', 1)
         self.timer = self.create_timer(1, self.timer_callback)
 
     def timer_callback(self):
@@ -39,7 +78,8 @@ class DetectBlueNode(Node):
     def image_callback(self, msg):
 
         # Log callback 
-        self.get_logger().info('Image received')
+        if self.verbose:
+            self.get_logger().info('Image received')
 
         # Convert the ROS Image message to a numpy array
         image = rnp.numpify(msg)
@@ -49,23 +89,29 @@ class DetectBlueNode(Node):
         # Apply the blue detection algorithm
 
         # Case 0: Simple HSV thresholding
+        debug_string = ''
         if self.detection_method == 0:
-            self.get_logger().info('Using simple HSV thresholding')
+            debug_string = 'Simple HSV thresholding'
             mask, thresholded_image, blue_detected  = BlueDetectionAlgorithm.simple_HSV_theshhold(image=image)
 
         # Case 1: HSV thresholding wih k-means clustering to remove sky
         elif self.detection_method == 1:
-            self.get_logger().info('Using HSV thresholding with k-means clustering to remove sky')
+            debug_string = 'HSV thresholding with k-means clustering to remove sky'
             mask, thresholded_image, blue_detected  = BlueDetectionAlgorithm.simple_HSV_theshhold_with_k_clustering_to_remove_sky(image=image)
 
         # Case 2: HSV thresholding with k-means and coverage thresholding
         elif self.detection_method == 2:
-            self.get_logger().info('Using HSV thresholding with k-means and coverage thresholding')
+            debug_string = 'HSV thresholding with k-means and coverage thresholding'
             mask, thresholded_image, blue_detected  = BlueDetectionAlgorithm.simple_HSV_theshhold_with_k_clustering_and_coverage_thresholding(image=image)
 
         # Default case: Simple HSV thresholding
         else:
+            debug_string = 'Simple HSV thresholding'
             mask, thresholded_image, blue_detected  = BlueDetectionAlgorithm.simple_HSV_theshhold(image=image)
+
+        # Log the debug string
+        if self.verbose:
+            self.get_logger().info(debug_string)
 
         # Conver images to BGR
         original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)
